d0d41d40951ec72cae57738f0dc2bb3fa43143b6
Pritam Halder <halderpritamhalder1@gmail.com>
2025-06-08 15:23:09 +0530

    preload

---
+++ Preload-Pattern/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <img
+        src="/image.png"
+        alt="Hero"
+        width="100%"
+        height="auto"
+        loading="eager"
+        style={{ maxHeight: '400px' }}
+      />
+    </div>
+  );
+}
+export default App;
